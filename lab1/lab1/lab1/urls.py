ce1660bfc9956f8d4903fb9dafd4e7a3dc8d882b
ST3505 <ST3505@TE.local>
2024-11-29 12:00:25 +0600

    ,t,t,t,

--- lab1/lab1/lab1/urls.py
+++ lab1/lab1/lab1/urls.py
@@ -24,6 +24,9 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('about/', views.about),
+    path('', views.index),
+    path('posts/', include('posts.urls')),
+    path('communities/', include('communities.urls')),
 ]
 
 
---
+++ lab1/lab1/posts/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def posts_list(req):
+    return render(req, 'posts/posts_list.html')
