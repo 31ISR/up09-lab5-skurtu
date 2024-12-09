9be2d6835cdba808316e95666e49ece4d0962c5e
ST3507 <ST3507@TE.local>
2024-12-09 17:33:27 +0600

    пампампампам

--- lab1/lab1/communities/models.py
+++ lab1/lab1/communities/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Communities(models.Model):
+class Communitie(models.Model):
     name = models.CharField(max_length=75)
     description = models.CharField(max_length=150)
     slug = models.SlugField()
@@ -8,4 +8,4 @@ class Communities(models.Model):
     free = models.BooleanField()
 
     def __str__(self):
-        return self.title
+        return self.name
--- lab1/lab1/communities/urls.py
+++ lab1/lab1/communities/urls.py
@@ -2,6 +2,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'communities'
+
 urlpatterns = [
-    path('', views.communities_list),
+    path('', views.communities_list, name="list"),
+    path('<slug:slug>', views.communitie_page, name="page")
 ]
