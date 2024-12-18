8385225c63eb9c89270b65fe8db95e10996e61b9
ST3614 <ST3614@TE.local>
2024-12-18 10:15:32 +0600

    лабудилабудай

--- lab1/lab1/lab1/urls.py
+++ lab1/lab1/lab1/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,5 +29,8 @@ urlpatterns = [
     path('posts/', include('posts.urls')),
     path('communities/', include('communities.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
